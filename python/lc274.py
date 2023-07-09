8b66c62218f5b9d022a0d1914a74a6ad31b5d080
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-09 15:14:33 +0100

    274(incomplete)

---
+++ python/lc274.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+
+
+citations = [3, 0, 6, 1, 5]
+s = Solution()
+print(s.hIndex(citations))
