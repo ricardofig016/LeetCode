3f0dd135d8506df6abe5fedda6480fe7cd45b41f
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-09 19:32:53 +0100

    274

--- python/lc274.py
+++ python/lc274.py
@@ -4,6 +4,13 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
+        h = 0
+        n = len(citations)
+        citations.sort(reverse=True)
+        for i in range(n):
+            if citations[i] >= i + 1:
+                h = i + 1
+        return h
 
 
 citations = [3, 0, 6, 1, 5]
