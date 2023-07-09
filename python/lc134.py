613d677b3e33ff62e5bfc1a421d2a48b1a65004c
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-09 23:10:23 +0100

    134(incomplete)

---
+++ python/lc134.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def idk_bro_site_broke(self, gas, cost):
+        return -1
+
+
+gas = [1, 2, 3, 4, 5]
+cost = [3, 4, 5, 1, 2]
+s = Solution()
+print(s.idk_bro_site_broke(gas, cost))
