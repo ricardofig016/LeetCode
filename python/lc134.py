0044017af73e8f3f896831d0a9bf334d0092f07a
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-10 23:34:41 +0100

    134, 135

--- python/lc134.py
+++ python/lc134.py
@@ -1,9 +1,28 @@
 class Solution(object):
-    def idk_bro_site_broke(self, gas, cost):
-        return -1
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+
+        n = len(gas)
+        diff = [gas[i] - cost[i] for i in range(n)]
+
+        if sum(diff) < 0:
+            return -1
+
+        total = 0
+        start = 0
+        for i in range(n):
+            total += diff[i]
+            if total < 0:
+                total = 0
+                start = i + 1
+        return start
 
 
 gas = [1, 2, 3, 4, 5]
 cost = [3, 4, 5, 1, 2]
 s = Solution()
-print(s.idk_bro_site_broke(gas, cost))
+print(s.canCompleteCircuit(gas, cost))
