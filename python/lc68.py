b8e53102d62e8059f8ef8ae7f15d0f5f646effaa
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-18 22:55:37 +0100

    68

--- python/lc68.py
+++ python/lc68.py
@@ -7,6 +7,7 @@ class Solution(object):
         """
         lines = []
         curr_line = ""
+
         for word in words:
             if len(word) <= maxWidth - len(curr_line):
                 curr_line += word + " "
@@ -17,7 +18,32 @@ class Solution(object):
             else:
                 lines.append(curr_line[:-1])
                 curr_line = word + " "
-        lines.append(curr_line[:-1])
+        if curr_line:
+            lines.append(curr_line[:-1])
+
+        for i in range(len(lines)):
+            missing = maxWidth - len(lines[i])
+            spaces = 0
+            for ch in lines[i]:
+                if ch == " ":
+                    spaces += 1
+            if spaces == 0 or i == len(lines) - 1:
+                lines[i] += " " * missing
+            else:
+                increment = missing // spaces
+                rest = missing % spaces
+                j = 0
+                space_counter = 0
+                while j < len(lines[i]):
+                    if lines[i][j] == " ":
+                        space_counter += 1
+                        lines[i] = lines[i][:j] + " " * increment + lines[i][j:]
+                        if space_counter <= rest:
+                            lines[i] = lines[i][:j] + " " + lines[i][j:]
+                            j += 1
+                        j += increment
+                    j += 1
+
         return lines
 
 
@@ -46,7 +72,10 @@ words3 = [
     "do",
 ]
 maxWidth3 = 20
+words4 = ["Here", "is", "an", "example", "of", "text", "justification."]
+maxWidth4 = 14
 s = Solution()
 print(s.fullJustify(words1, maxWidth1))
 print(s.fullJustify(words2, maxWidth2))
 print(s.fullJustify(words3, maxWidth3))
+print(s.fullJustify(words4, maxWidth4))
