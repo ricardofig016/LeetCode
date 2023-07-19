9bf652800c3a5c709023a07889ec908fb648400c
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-19 20:19:38 +0100

    209(imcomplete)

---
+++ python/lc209.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+target = 7
+nums = [2, 3, 1, 2, 4, 3]
+s = Solution()
+print(s.minSubArrayLen(target, nums))
