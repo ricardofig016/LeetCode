5e2c4a3f9ce6ce35c0473154e2c87d3e3560385c
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-31 16:37:48 +0100

    209

--- python/lc209.py
+++ python/lc209.py
@@ -5,6 +5,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n = len(nums)
+        best_size = n + 1
+        s, e = (0, 0)
+        curr_window_sum = nums[0]
+        while s < n and e < n:
+            if curr_window_sum < target:
+                e += 1
+                if e < n:
+                    curr_window_sum += nums[e]
+            else:
+                best_size = min(best_size, e - s + 1)
+                if best_size == 1:
+                    return best_size
+                curr_window_sum -= nums[s]
+                s += 1
+        return 0 if best_size == n + 1 else best_size
 
 
 target = 7
